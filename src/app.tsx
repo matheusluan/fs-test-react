a29d8f90511c9044dca5595c5010ca3c1cf2cf68
Matheus Luan <matheusbrito1999@live.com>
2024-03-10 14:25:15 -0300

    ajustment

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,16 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Home } from './pages/home';
 import { Play } from './pages/play';
 
 export function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/play" element={<Play />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
