93ddc23429f842a1636d8c3783ce55230c0c2014
Matheus Luan <matheusbrito1999@live.com>
2024-03-10 23:13:27 -0300

    Adjustment

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,8 +1,12 @@
 import { Button } from "./button";
+import backgroundImage from '../assets/background.png';
 
 export function Banner() {
     return (
-        <div className="relative w-full h-96 bg-no-repeat rounded-lg bg-cover bg-cassino-background">
+        <div
+            style={{ backgroundImage: `url(${backgroundImage})` }}
+            className="relative w-full h-96 bg-no-repeat rounded-lg bg-cover"
+        >
             <div className='absolute top-40 left-8'>
                 <h1 className='text-4xl font-bold'>You already have 20 coins</h1>
                 <span className='font-extralight text-gray-50'>Start now and receive 20 coins for slot machine.</span>
